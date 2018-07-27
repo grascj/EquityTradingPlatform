ec3319b0e37a9efa69dabeb9bc797604088c0de6
Chris Grasing <christopher.grasing@gmail.com>
2018-07-27 13:29:37 +0000

    example reading from market feed

--- AlgoTradingApp/src/main/java/com/citi/training/Application.java
+++ AlgoTradingApp/src/main/java/com/citi/training/Application.java
@@ -3,9 +3,13 @@ package com.citi.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan
+@EnableScheduling
 @EnableMongoRepositories(basePackages = "com.citi.training")
 public class Application {
 
---
+++ AlgoTradingApp/src/main/java/com/citi/training/feed/FeedReader.java
@@ -0,0 +1,47 @@
+package com.citi.training.feed;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+@Service
+public class FeedReader {
+
+    @Scheduled(fixedRate = 1000)
+    public void pingFeed(){
+        URL url = null;
+        try {
+            url = new URL("http://localhost:8085/quotes.csv?s=goog&f=p0");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            System.out.println(content);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
