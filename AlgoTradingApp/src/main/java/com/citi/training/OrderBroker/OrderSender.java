f46a7c732f202dd80b3383ec62e80af98bd44e72
alex bae <ajsbae@gmail.com>
2018-07-27 20:30:51 +0000

    broker message db connection

--- AlgoTradingApp/src/main/java/com/citi/training/OrderBroker/OrderSender.java
+++ AlgoTradingApp/src/main/java/com/citi/training/OrderBroker/OrderSender.java
@@ -47,6 +47,6 @@ public class OrderSender {
                 return msg;
             }
         };
-        jmsTemplate.send("OrderBroker ", messageCreator);
+        jmsTemplate.send("OrderBroker", messageCreator);
     }
 }
--- AlgoTradingApp/src/main/java/com/citi/training/repositories/BrokerMessageRepository.java
+++ AlgoTradingApp/src/main/java/com/citi/training/repositories/BrokerMessageRepository.java
@@ -4,6 +4,6 @@ package com.citi.training.repositories;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CompletedOrderRepository extends MongoRepository<com.citi.training.entities.BrokerMessage, ObjectId> {
+public interface BrokerMessageRepository  extends MongoRepository<com.citi.training.entities.BrokerMessage, ObjectId> {
 
 }
