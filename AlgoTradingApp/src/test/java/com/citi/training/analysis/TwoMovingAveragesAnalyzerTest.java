5e63f151e47d9c0b16b8e16b849a7809c2f20a3f
Chris Grasing <christopher.grasing@stonybrook.edu>
2018-07-30 13:50:19 +0000

    added testing redependencies

---
+++ AlgoTradingApp/src/test/java/com/citi/training/analysis/TwoMovingAveragesAnalyzerTest.java
@@ -0,0 +1,10 @@
+package com.citi.training.analysis;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoMovingAveragesAnalyzerTest {
+
+    @org.junit.jupiter.api.Test
+    void analyze() {
+    }
+}
