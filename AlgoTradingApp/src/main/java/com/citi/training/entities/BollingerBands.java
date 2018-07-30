cadfd25e5409a8bd415725d4651d987df6411541
Christopher Grasing <cgrasing@yahoo.com>
2018-07-30 14:59:17 +0000

    patch bollinger bands constructor

--- AlgoTradingApp/src/main/java/com/citi/training/entities/BollingerBands.java
+++ AlgoTradingApp/src/main/java/com/citi/training/entities/BollingerBands.java
@@ -8,7 +8,7 @@ public class BollingerBands extends Strategy {
     Double test;
 
     public BollingerBands(String ticker, Double test) {
-        super(ticker);
+        super(null, null, null, null);
         this.test = test;
     }
 
