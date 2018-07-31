5e2c7a07677732370a682a886d6112496a985044
RabbaniR <rnr254@nyu.edu>
2018-07-31 22:59:41 +0000

    get trends from past hr only

--- TradingAppWebInterface/src/main/java/com/citi/training/entities/Trade.java
+++ TradingAppWebInterface/src/main/java/com/citi/training/entities/Trade.java
@@ -1,13 +1,28 @@
 package com.citi.training.entities;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "trades")
+
 public class Trade {
-    Order order;
-    String result;
+    @Id
+    private ObjectId id;
+    private Order order;
+    private String result;
+    private String strategyId;
+    private Double profitAndLoss;
+    private LocalDateTime timeStamp;
 
-    public Trade(Order order, String result){
+    public Trade(Order order, String result, String strategyId, Double profitAndLoss) {
         this.order = order;
-        this.result  = result;
-
+        this.result = result;
+        this.strategyId = strategyId;
+        this.profitAndLoss = profitAndLoss;
+        this.timeStamp = LocalDateTime.now();
     }
 
     public Order getOrder() {
@@ -25,4 +40,26 @@ public class Trade {
     public void setResult(String result) {
         this.result = result;
     }
+
+    public String getId() {
+        return strategyId;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Double getProfitAndLoss() {
+        return profitAndLoss;
+    }
+
+    public void setProfitAndLoss(Double profitAndLoss) {
+        this.profitAndLoss = profitAndLoss;
+    }
+
+    public LocalDateTime getTimeStamp() { return timeStamp;   }
+
+    public void setTimeStamp(LocalDateTime timeStamp) { this.timeStamp = timeStamp; }
+
+
 }
