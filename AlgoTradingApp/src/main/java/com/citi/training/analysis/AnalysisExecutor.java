dc861985acb4b686b6a3e0a16045578db32f98c0
alex bae <ajsbae@gmail.com>
2018-08-03 18:45:59 +0000

    comments and docs

--- AlgoTradingApp/src/main/java/com/citi/training/analysis/AnalysisExecutor.java
+++ AlgoTradingApp/src/main/java/com/citi/training/analysis/AnalysisExecutor.java
@@ -20,6 +20,11 @@ public class AnalysisExecutor {
     private TwoMovingAveragesAnalyzer twoMovingAveragesAnalyzer;
     private Map<Class, Analyzer> analyzerMap;
 
+    /**
+     * Takes a strategy from the analyzer map and runes the analyze method on each strategy
+     * @param strategy a strategy of a  particular type given by the analyzer map
+     * @return
+     */
     public Order execute(Strategy strategy) {
         return analyzerMap.get(strategy.getClass()).analyze(strategy);
     }
--- AlgoTradingApp/src/main/java/com/citi/training/analysis/Analyzer.java
+++ AlgoTradingApp/src/main/java/com/citi/training/analysis/Analyzer.java
@@ -6,8 +6,22 @@ import com.citi.training.misc.Action;
 
 public abstract class Analyzer {
 
+    /**
+     * An abstract method that executes the logic in each strategy
+     * @param strat
+     * @return
+     */
     abstract Order analyze(Strategy strat);
 
+    /**
+     * A strategy that checks to see if a strategy has met it's exit condition.
+     * Default condition is a 20% loss
+     * @param strat the strat that the exit condition is checking for
+     * @param price a price variable that can be checked on live prices (not yet implemented )
+     * @param exitPercentage the percentage that the strategy should exit at
+     * @param exitRule whether or no the strat should exit at a profit or loss
+     * @return a boolean that says whether or not the strat should exit
+     */
     public Boolean shouldExit(Strategy strat, Double price, Double exitPercentage, String exitRule) {
 
         if (strat.getProfitAndLoss() < strat.getInitialvalue() * .8) {
